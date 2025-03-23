9c8875a211dfada37de1efaac9ef102ea9f05158
mrkobold <kovacsboldizsar@gmail.com>
2025-03-23 18:33:29 +0200

    progress

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 10443
+const fs = require('fs')
+var http = require('http');
+var https = require('https');
+const express = require('express');
+const { readConfig } = require('./readconfig');
 
-app.get('/test', (req, res) => {
-  res.send('timestamp from backend: ' + Date.now())
-})
+const config = readConfig('./config/config_local.json')
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+var startup = async () => {
+  const isSSL = config.sslFullchain && config.sslPrivkey
+  const app = express()
+
+  if (isSSL) {
+    var certificate = fs.readFileSync(config.sslFullchain, 'utf8');
+    var privateKey = fs.readFileSync(config.sslPrivkey, 'utf8');
+    var credentials = { key: privateKey, cert: certificate };
+    var httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(10443);
+    console.log(`initialized https express server, listening on port ${config.port}`)
+  } else {
+    var httpServer = http.createServer(app);
+    httpServer.listen(10443);
+    console.log(`initialized http express server, listening on port ${config.port}`)
+  }
+
+  setupEndpoints(app)
+}
+
+var setupEndpoints = (app) => {
+  app.get('/test', (req, res) => {
+    res.send('timestamp from backend: ' + Date.now())
+  })
+}
+
+
+startup()
---
+++ backend/src/readconfig.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+const readConfig = function (path) {
+    console.log(`reading config file at: ${path}`)
+    const configFileContent = fs.readFileSync(path)
+    const configMap = JSON.parse(configFileContent)
+
+    console.log('server configurations ***********************')
+    for (const [key, val] of Object.entries(configMap)) {
+        console.log(`${key}: ${val}`)
+    }
+    console.log('server configurations ***********************')
+
+    return configMap
+}
+
+module.exports = { readConfig } 
