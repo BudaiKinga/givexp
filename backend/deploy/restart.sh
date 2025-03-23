9cd5c0f40d1d58df68a8ef995cec4c2ac9b10c23
mrkobold <kovacsboldizsar@gmail.com>
2025-03-23 18:37:42 +0200

    Progress

---
+++ backend/deploy/restart.sh
@@ -0,0 +1,12 @@
+docker stop backend
+docker remove backend
+
+docker build -f ./deploy/Dockerfile -t backend .
+
+
+docker run \
+	--name backend \
+    -p 10443:10443 \
+	--mount src=/home/kbk/givexp/backend/config/config_remote.json,target=/usr/src/app/config.json,type=bind \
+	--mount src=/home/kbk/certs/,target=/etc/ssl/ui_certs/,type=bind \
+	-d backend
