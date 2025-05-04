9a238cee5501a8b131d0861a327b2a1aa88d9522
mrkobold <kovacsboldizsar@gmail.com>
2025-05-04 18:59:29 +0300

    updated review mesage

--- frontend/src/components/Homepage/Reviews/Reviews.jsx
+++ frontend/src/components/Homepage/Reviews/Reviews.jsx
@@ -8,36 +8,36 @@ const Reviews = () => {
       <div className="title">Értékeléseink</div>
       <div className="list">
         <div className="entry">
-          <div className="quote">"Fasza volt, ajánlom"</div>
+          <div className="quote">"Szuper volt, ajánlom"</div>
           <div className="whom">Gabi, 21</div>
           <div className="where">E-bike túra</div>
           <img src={stars} />
         </div>
         <div className="entry">
-          <div className="quote">"Fasza volt, ajánlom"</div>
+          <div className="quote">"Szuper volt, ajánlom"</div>
           <div className="whom">Gabi, 21</div>
           <div className="where">E-bike túra</div>
           <img src={stars} />
         </div>
         <div className="entry">
-          <div className="quote">"Fasza volt, ajánlom"</div>
+          <div className="quote">"Szuper volt, ajánlom"</div>
           <div className="whom">Gabi, 21</div>
           <div className="where">E-bike túra</div>
           <img src={stars} />
         </div> <div className="entry">
-          <div className="quote">"Fasza volt, ajánlom"</div>
+          <div className="quote">"Szuper volt, ajánlom"</div>
           <div className="whom">Gabi, 21</div>
           <div className="where">E-bike túra</div>
           <img src={stars} />
         </div>
         <div className="entry">
-          <div className="quote">"Fasza volt, ajánlom"</div>
+          <div className="quote">"Szuper volt, ajánlom"</div>
           <div className="whom">Gabi, 21</div>
           <div className="where">E-bike túra</div>
           <img src={stars} />
         </div>
         {/* <div className="entry">
-          <div className="quote">"Fasza volt, ajánlom"</div>
+          <div className="quote">"Szuper volt, ajánlom"</div>
           <div className="whom">Gabi, 21</div>
           <div className="where">E-bike túra</div>
           <img src={stars} />
